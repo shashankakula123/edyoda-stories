b54630f0530c1c4d63daf157f7ab1cb1ba4a496b
shashankakula123 <61818238+shashankakula123@users.noreply.github.com>
2021-05-20 14:17:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+<div id="left">
+        <div id="first">
+          <a id="edyoda">EDYODA</a>
+          <p id="stories">Stories</p>
+         
+        </div>
+
+        <div id="explore">
+        <a>Explore Catogories </a>
+        <a><img class="BlogNavigationLink__DropdownArrow__2Uzyk" alt="Dropdown Icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAGCAYAAAD37n+BAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAHhJREFUeNpiYGBguAPE/f///2fAh0FqgPguiBEDxC+BeDUQM2NRyAyVewlWCxW0B+L7QHwAiHmRFPNCxUBy9mAxJElVIL4MxBeBWB6KL0LFVOHq0KwXAuJDQHwPikFsIWQ1jFCFcMDIyMgGpFZAuRFA+V/I8gABBgD8EGd4shdx5wAAAABJRU5ErkJggg=="/></a>
+        </div>
+</div>
+
+
+
+<div id="right">
+        <div>
+          <p>EdYoda is free learning and knowledge<br/>
+sharing platform for techies</p>
+
+        </div>
+
+
+        <div>
+          <button id="goto">Go To The Main Website</button>
+          </div>
+</div>
+
+      </header>
+
+        <main>
+        <p>Latest Posts
+
+        </p>
+
+        <div id="filter" >
+        <img  alt="Filter Icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAbFBMVEUAAAA1gME1gME1f8A1gME1gMExd7Q0f8AxeLYzfb00fr80f8Aze7oze7o1gMA0fr4zfLwzfb0zfb0yerg0fb00f8AzfLszfLwzfr40f780f8Azfb00f8A0f8AzfLwzfr81gME0fr4zfLs1gMEkRxAZAAAAI3RSTlMAHOP9f/oT1RAO281OROfhFaSUSkCwnpmOcTfFim9YB/AmGRclE4kAAAF9SURBVGje7dfrboJAEAXgA7jLXRAExRu2vP87No1NJjYpexm3SZv5/vgD9QB7mCwQQggh/rI2YtCwUpUxw7mGhTZdWMoWZlHMC4kjmL0Xv3Al8zHhZCQTbOid159/fRwyWIlK+p2rQcPSvnBLoa+mJ1jrG7+bVdRwcLsvHpoeLuaLepxh4nAd9xvc5F1ityx0Juoyw5EeXZely2DAL/JOw8M+NafQ4TKCl7pxeED28FRtrctbw9uk1u4YHdhW8Jd39LislXcCRzZalEt1OVj0YF76MQPTyVjkQYOtprlvmO4cfbysoOnOc6UiG6Y7Q36kIn8v7xWvkh1+mPvqmIPBtIExb0385z5/ups3MM+2b3ixi3nXy7eREAmREAmREAmRkPWQsw4Xkiz05hMqJEkoI0wIZTQVEC7k8VH0CGDztB5pDRKkXfTWHjJkOMGAX+Fdi0CowqMGCROiDpQRKkR1GcLZ0PtbODf1OUquOULKpzQ+VwitjTIIIYQQ/9wHYyfu5wm8dM0AAAAASUVORK5CYII="/>
+          
+          <a>Filter by Category</a>
+          </div>
+
+
+          <div id="lists">
+
+             <a class="all">All</a>
+             <a class="all">Artificial Intelligence</a>
+             <a class="all">Cloud Computing</a>
+             <a class="all">DevOps</a>
+             <a class="all">Programming Languages</a>
+             <a class="all">Mobile Application Development</a>
+             
+             
+             <a class="all">Technology and Tools</a>
+             <a class="all">Get a Job in a Tech Company</a>
+             <a class="all">Others</a>
+
+
+            </div>
+
+        </main>
+
+    </div>
+  );
+}
+
+export default App;
